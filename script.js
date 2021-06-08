d048b7fe8f112926af5f454b7dd30461de4dc070
TinekeKvdW <84176956+TinekeKvdW@users.noreply.github.com>
2021-06-08 11:30:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const muurVerven = function (site, color) {
+    console.log("De " + site + " muur dient " + color + " te worden geverfd.");
+};
+
+const siteone = "noord";
+const sitetwo = "zuid";
+const colorone = "rood";
+const colortwo = "blauw";
+
+muurVerven(siteone, colorone);
+muurVerven(sitetwo, colortwo);
